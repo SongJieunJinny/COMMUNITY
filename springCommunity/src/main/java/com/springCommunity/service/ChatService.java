77204cc781989ccb0e06c8ec3b782ce0c54d5817
iseuljang <sgktn0204@gmail.com>
2024-12-06 12:46:38 +0900

    채팅방 생성, 생성시 초대회원 조회 및 선택, 채팅방 목록

---
+++ springCommunity/src/main/java/com/springCommunity/service/ChatService.java
@@ -0,0 +1,14 @@
+package com.springCommunity.service;
+
+import java.util.List;
+
+import com.springCommunity.vo.*;
+
+public interface ChatService {
+	
+	public int selectTotal(SearchVO searchVO);
+	public List<ChatVO> selectAll(SearchVO searchVO);
+	public List<UserInfoVO> searchUsers(SearchVO searchVO);
+	public int insertRoom(ChatVO chatVO);
+	public void insertRoomAfterUser(ChatVO chatVO);
+}
