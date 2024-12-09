e5f6a0dc97cb4290038e7b8b8603e3ec9fc2a1e7
leedongyoon <tank102124@naver.com>
2024-12-09 14:48:03 +0900

    이동윤 notice commit

---
+++ springCommunity/src/main/java/com/springCommunity/controller/Notice2Controller.java
@@ -0,0 +1,10 @@
+package com.springCommunity.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RequestMapping(value="/login/notice")
+@Controller
+public class Notice2Controller {
+
+}
